609a3c8371066bd2f843e893486c960777a85c03
Swackyy <68652827+Swackyy@users.noreply.github.com>
2025-01-12 14:08:00 +0000

    Fix items, again
    How do I keep unfixing the same bug???

--- loader/forge/src/main/java/com/swacky/ohmega/common/init/OhmegaItems.java
+++ loader/forge/src/main/java/com/swacky/ohmega/common/init/OhmegaItems.java
@@ -2,22 +2,25 @@ package com.swacky.ohmega.common.init;
 
 import com.swacky.ohmega.common.item.AngelRing;
 import com.swacky.ohmega.common.OhmegaCommon;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class OhmegaItems {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, OhmegaCommon.MODID);
 
     // This is simply a test and example accessory. Its code may be used as a reference to create your own.
-    public static final RegistryObject<Item> ANGEL_RING = register("angel_ring", () -> new AngelRing(new Item.Properties().stacksTo(1)));
+    public static final RegistryObject<Item> ANGEL_RING = register("angel_ring", AngelRing::new, new Item.Properties().stacksTo(1));
 
-    private static <T extends Item> RegistryObject<T> register(String id, Supplier<T> sup) {
-        return ITEMS.register(id, sup);
+    private static <T extends Item> RegistryObject<T> register(String id, Function<Item.Properties, T> function, Item.Properties props) {
+        return ITEMS.register(id, () -> function.apply(props.setId(ResourceKey.create(Registries.ITEM, OhmegaCommon.rl(id)))));
     }
 
     public static void register(IEventBus bus) {
--- loader/neoforge/src/main/java/com/swacky/ohmega/common/init/OhmegaItems.java
+++ loader/neoforge/src/main/java/com/swacky/ohmega/common/init/OhmegaItems.java
@@ -6,16 +6,17 @@ import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class OhmegaItems {
     private static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(OhmegaCommon.MODID);
 
     // This is simply a test and example accessory. Its code may be used as a reference to create your own.
-    public static final Supplier<Item> ANGEL_RING = register("angel_ring", () -> new AngelRing(new Item.Properties().stacksTo(1)));
+    public static final Supplier<Item> ANGEL_RING = register("angel_ring", AngelRing::new, new Item.Properties().stacksTo(1));
 
-    private static <T extends Item> Supplier<T> register(String id, Supplier<T> sup) {
-        return ITEMS.register(id, sup);
+    private static <T extends Item> Supplier<T> register(String id, Function<Item.Properties, T> function, Item.Properties props) {
+        return ITEMS.registerItem(id, function, props);
     }
 
     public static void register(IEventBus bus) {
