40db6d39d7a3a2c130127be2a70db6b8e0d79d29
Swackyy <68652827+Swackyy@users.noreply.github.com>
2026-01-15 09:15:10 +0000

    1.5.10
    Some small rendering optimisations and alignment with other versions

--- loader/forge/src/main/java/com/swacky/ohmega/api/event/AccessoryCanUnequipEvent.java
+++ loader/forge/src/main/java/com/swacky/ohmega/api/event/AccessoryCanUnequipEvent.java
@@ -9,29 +9,13 @@ import org.jspecify.annotations.NonNull;
 public final class AccessoryCanUnequipEvent extends MutableEvent {
     public static final EventBus<@NonNull AccessoryCanUnequipEvent> BUS = EventBus.create(AccessoryCanUnequipEvent.class);
 
-    private final Player player;
-    private final ItemStack stack;
-    private boolean ret;
+    public final Player player;
+    public final ItemStack stack;
+    public boolean returnValue;
 
     public AccessoryCanUnequipEvent(Player player, ItemStack stack, boolean flag) {
         this.player = player;
         this.stack = stack;
-        this.ret = flag;
-    }
-
-    public Player getPlayer() {
-        return player;
-    }
-
-    public ItemStack getStack() {
-        return stack;
-    }
-
-    public boolean getReturnValue() {
-        return this.ret;
-    }
-
-    public void setReturnValue(boolean value) {
-        this.ret = value;
+        this.returnValue = flag;
     }
 }
--- loader/neoforge/src/main/java/com/swacky/ohmega/api/event/AccessoryOverrideTypesEvent.java
+++ loader/neoforge/src/main/java/com/swacky/ohmega/api/event/AccessoryOverrideTypesEvent.java
@@ -1,6 +1,5 @@
 package com.swacky.ohmega.api.event;
 
-import com.google.common.collect.ImmutableMap;
 import com.swacky.ohmega.common.accessorytype.AccessoryType;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.Event;
@@ -9,14 +8,5 @@ import net.neoforged.fml.event.IModBusEvent;
 import java.util.WeakHashMap;
 
 public final class AccessoryOverrideTypesEvent extends Event implements IModBusEvent {
-    private final WeakHashMap<Item, AccessoryType> overrideRemaps = new WeakHashMap<>();
-
-    public AccessoryOverrideTypesEvent put(Item item, AccessoryType type) {
-        this.overrideRemaps.put(item, type);
-        return this;
-    }
-
-    public ImmutableMap<Item, AccessoryType> get() {
-        return ImmutableMap.copyOf(this.overrideRemaps);
-    }
+    public final WeakHashMap<Item, AccessoryType> overrideRemaps = new WeakHashMap<>();
 }
