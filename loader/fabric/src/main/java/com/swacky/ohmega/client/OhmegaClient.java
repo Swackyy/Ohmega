179dae8dcfe1b55cfac28d4390f3c97e72546205
Swackyy <68652827+Swackyy@users.noreply.github.com>
2026-03-25 07:28:08 +0000

    finish 26.1 port

--- loader/fabric/src/main/java/com/swacky/ohmega/client/OhmegaClient.java
+++ loader/fabric/src/main/java/com/swacky/ohmega/client/OhmegaClient.java
@@ -2,6 +2,7 @@ package com.swacky.ohmega.client;
 
 import com.swacky.ohmega.client.screen.AccessoryInventoryScreen;
 import com.swacky.ohmega.common.OhmegaCommon;
+import com.swacky.ohmega.common.init.OhmegaBinds;
 import com.swacky.ohmega.common.init.OhmegaMenusImpl;
 import com.swacky.ohmega.config.OhmegaConfigImpl;
 import com.swacky.ohmega.event.ClientEvents;
@@ -11,6 +12,7 @@ import com.swacky.ohmega.network.S2C.SyncAccessoryTypesPacket;
 import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
 import fuzs.forgeconfigapiport.fabric.api.v5.client.ConfigScreenFactoryRegistry;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientConfigurationNetworking;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.gui.screens.MenuScreens;
@@ -26,6 +28,7 @@ public final class OhmegaClient implements ClientModInitializer {
 
         ConfigRegistry.INSTANCE.register(OhmegaCommon.MODID, ModConfig.Type.CLIENT, OhmegaConfigImpl.Client.getSpec());
 
+        KeyMappingHelper.registerKeyMapping(OhmegaBinds.OPEN_ACC_INV);
         MenuScreens.register(OhmegaMenusImpl.ACCESSORY_INVENTORY, AccessoryInventoryScreen::new);
 
         ClientPlayNetworking.registerGlobalReceiver(SyncAccessorySlotsPacket.TYPE, OhmegaNetworkingImpl.S2C::handleSyncAccessorySlots);
