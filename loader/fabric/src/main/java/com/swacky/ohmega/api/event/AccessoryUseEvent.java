e6270b1cb9f3d68c9cf910eeabb2e33deaf884e6
Swackyy <68652827+Swackyy@users.noreply.github.com>
2026-04-12 17:20:35 +0100

    random fixes

--- loader/fabric/src/main/java/com/swacky/ohmega/api/event/AccessoryUseEvent.java
+++ loader/fabric/src/main/java/com/swacky/ohmega/api/event/AccessoryUseEvent.java
@@ -2,14 +2,14 @@ package com.swacky.ohmega.api.event;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 
 public interface AccessoryUseEvent {
     Event<AccessoryUseEvent> EVENT = EventFactory.createArrayBacked(AccessoryUseEvent.class,
-        listeners -> (player, stack) -> {
+        listeners -> (entity, stack) -> {
             for (AccessoryUseEvent listener : listeners) {
-                if (listener.process(player, stack)) {
+                if (listener.process(entity, stack)) {
                     return true;
                 }
             }
@@ -18,5 +18,5 @@ public interface AccessoryUseEvent {
         }
     );
 
-    boolean process(Entity entity, ItemStack stack);
+    boolean process(LivingEntity entity, ItemStack stack);
 }
--- loader/neoforge/src/main/java/com/swacky/ohmega/api/event/AccessoryCanEquipEvent.java
+++ loader/neoforge/src/main/java/com/swacky/ohmega/api/event/AccessoryCanEquipEvent.java
@@ -1,17 +1,17 @@
 package com.swacky.ohmega.api.event;
 
 import com.swacky.ohmega.api.EquipContext;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.Event;
 
 public final class AccessoryCanEquipEvent extends Event {
-    public final Entity entity;
+    public final LivingEntity entity;
     public final ItemStack stack;
     public final EquipContext context;
     public boolean returnValue;
 
-    public AccessoryCanEquipEvent(Entity entity, ItemStack stack, EquipContext context, boolean original) {
+    public AccessoryCanEquipEvent(LivingEntity entity, ItemStack stack, EquipContext context, boolean original) {
         this.entity = entity;
         this.stack = stack;
         this.context = context;
