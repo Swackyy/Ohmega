2cd54e4e7744030ac03d8d29a39698a531ff5b71
Swackyy <68652827+Swackyy@users.noreply.github.com>
2026-05-05 19:01:01 +0100

    use synthetic injector target
    more precise and fixes a ghosting bug

--- loader/common/src/main/java/com/swacky/ohmega/mixin/client/MultiPlayerGameModeMixin.java
+++ loader/common/src/main/java/com/swacky/ohmega/mixin/client/MultiPlayerGameModeMixin.java
@@ -1,39 +1,42 @@
 package com.swacky.ohmega.mixin.client;
 
-import com.swacky.ohmega.api.common.item.AccessoryHelper;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.swacky.ohmega.api.common.item.Accessories;
+import com.swacky.ohmega.api.common.item.AccessoryHelper;
 import com.swacky.ohmega.common.item.Accessory;
 import net.minecraft.client.multiplayer.MultiPlayerGameMode;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 // todo: see if this can be moved to one ItemStackMixin or something
 @Mixin(MultiPlayerGameMode.class)
 abstract class MultiPlayerGameModeMixin {
-    // todo: maybe use a different injector? Slightly more difficult here because synthetic targets are less reliable
-    @Inject(
-            method = "useItem",
+    @WrapOperation(
+            method = "lambda$useItem$0",
             at = @At(
-                    value = "RETURN"),
-            cancellable = true)
-    private void useItem(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
-        if (!cir.getReturnValue().consumesAction()) {
-            ItemStack stack = player.getItemInHand(hand);
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;"))
+    private InteractionResult useItem(ItemStack stack, Level level, Player player, InteractionHand hand, Operation<InteractionResult> handle) {
+        InteractionResult original = handle.call(stack, level, player, hand);
+
+        if (!original.consumesAction()) {
             Accessory accessory = Accessories.get(stack.getItem());
 
             if (accessory != null && accessory.preferVanillaUse(stack)) {
                 InteractionResult candidate = AccessoryHelper.tryEquip(player, stack);
 
                 if (candidate.consumesAction()) {
-                    cir.setReturnValue(candidate);
+                    return candidate;
                 }
             }
         }
+
+        return original;
     }
 }
