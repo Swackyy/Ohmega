da26145273955a8f222d701a33a5a3d0f1d74886
Swackyy <68652827+Swackyy@users.noreply.github.com>
2026-05-05 18:02:49 +0100

    update dispenserblockmixin injector
    it shouldve been included with the last commit but wasnt :p

--- loader/common/src/main/java/com/swacky/ohmega/mixin/DispenserBlockMixin.java
+++ loader/common/src/main/java/com/swacky/ohmega/mixin/DispenserBlockMixin.java
@@ -1,27 +1,27 @@
 package com.swacky.ohmega.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.swacky.ohmega.api.common.item.Accessories;
 import com.swacky.ohmega.common.block.dispenser.AccessoryDispenseItemBehaviour;
 import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DispenserBlock.class)
 public class DispenserBlockMixin {
-    @Inject(
+    @WrapOperation(
             method = "getDispenseMethod",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/block/DispenserBlock;getDefaultDispenseMethod(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/core/dispenser/DispenseItemBehavior;"),
-            cancellable = true)
-    public void getDispenseMethod(Level level, ItemStack stack, CallbackInfoReturnable<DispenseItemBehavior> cir) {
+                    target = "Lnet/minecraft/world/level/block/DispenserBlock;getDefaultDispenseMethod(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/core/dispenser/DispenseItemBehavior;"))
+    public DispenseItemBehavior getDispenseMethod(ItemStack stack, Operation<DispenseItemBehavior> handle) {
         if (Accessories.isBound(stack.getItem())) {
-            cir.setReturnValue(AccessoryDispenseItemBehaviour.getInstance());
+            return AccessoryDispenseItemBehaviour.getInstance();
         }
+
+        return handle.call(stack);
     }
 }
